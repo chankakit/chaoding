52c8cc10dfe81f8c9d022da6ce7cec56e8a117cc
chankaikt <chankakit@live.com>
2021-09-22 00:45:36 +0800

    添加计算 rps

--- back/calculate_rps/cal_rps.py
+++ back/calculate_rps/cal_rps.py
@@ -5,24 +5,32 @@ import os
 import pandas as pd
 import numpy as np
 import sqlite3 as sql
+from datetime import datetime
 
-if __name__=='__main__':
-  RPS_DAYS = (5, 10, 20, 60, 120, 250)
-  db_columns = ['company_code', 'bk']
-  for rps in RPS_DAYS:
-    db_columns.append(f'change_{rps}_pct')
-  # print(db_columns)
+def get_new_and_st(table, db_conn):
+  fields = '*'
+  # 筛选出不是一年内的新股且不为 ST
+  # conditions = 'listing_date < date("now","-1 years") AND instr(company_abbr, "ST") = 0'
+  # 筛选出上市不足一年或 ST
+  conditions = 'listing_date > date("now","-1 years") OR instr(company_abbr, "ST") = 1'
+  sql_string = (f'SELECT {fields} '
+                f'FROM {table} '
+                f'WHERE {conditions};')
+
+  stock_without_new_and_st = pd.read_sql(sql_string, db_conn)
+  return stock_without_new_and_st
+
+def cal_price_change(rps_days):
+  # RPS_DAYS = (5, 10, 20, 60, 120, 250)
+  # 构造列
   # ['company_code', 'bk', 'change_5_pct', 'change_10_pct', 'change_20_pct', 'change_60_pct', 'change_120_pct', 'change_250_pct']
+  db_columns = ['company_code', 'bk']
+  for rps in rps_days:
+    db_columns.append(f'change_{rps}_pct')
 
   db_dir_path = '../database/stock_a_history/'
   db_files = ('sh_zhuban', 'sh_kechuangban', 'sz_zhuban', 'sz_chuangyeban')
-  # db_file = 'sh_zhuban'
-  change_db_dir_path = '../database/rps/'
-  change_db_file = 'all_change'
 
-  if not os.path.exists(change_db_dir_path):
-    print('rps dir not exist, create one')
-    os.mkdir(change_db_dir_path)
   
   stocks_change_data = []
 
@@ -32,39 +40,101 @@ if __name__=='__main__':
       cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
       stocks = cursor.fetchall()
 
-      with sql.connect(change_db_dir_path + change_db_file + '.db') as change_db_conn:
-        
-        for stock in stocks:
-          # print(stock[0])
+      for stock in stocks:
+        # 初始化每个股票数据数组
+        # 根据上面的构造列顺序，第 25 行
+        # 因为历史数据每个表的表名是 交易所所在地两位拼音首字母+数字，所以 company_code 要去到拼音
+        column_data = [stock[0][2:8], db_file]
+
+        filed = '收盘'
+        # 获取最新日前的收盘价
+        sql_str_latest = f'SELECT 1, {filed} FROM {stock[0]} ORDER BY date("日期") DESC LIMIT 1'
+        stock_latest_price_df = pd.read_sql(sql_str_latest, conn)
+
+        for rps_day in rps_days:
+          day = rps_day
+          # 读取 x 日前的收盘价
+          sql_str_before = f'SELECT {day}, {filed} FROM {stock[0]} ORDER BY date("日期") DESC LIMIT 1 OFFSET {day}'
+          stock_x_day_price_df = pd.read_sql(sql_str_before, conn)
           
-          column_data = [stock[0], db_file]
 
-          filed = '收盘'
-          # 选择最新日前的收盘价
-          sql_str_latest = f'SELECT 1, {filed} FROM {stock[0]} ORDER BY date("日期") DESC LIMIT 1'
-          stock_latest_price_df = pd.read_sql(sql_str_latest, conn)
+          if not stock_x_day_price_df.empty:
+            stock_latest_price = stock_latest_price_df.iloc[0][filed]
+            stock_x_day_price = stock_x_day_price_df.iloc[0][filed]
 
-          for rps_day in RPS_DAYS:
-            day = rps_day
-            # 选择 x 日前的收盘价
-            sql_str_before = f'SELECT {day}, {filed} FROM {stock[0]} ORDER BY date("日期") DESC LIMIT 1 OFFSET {day}'
-            stock_x_day_price_df = pd.read_sql(sql_str_before, conn)
-            
-            # print(stock_x_day_price)
-            if not stock_x_day_price_df.empty:
-              stock_latest_price = stock_latest_price_df.iloc[0][filed]
-              stock_x_day_price = stock_x_day_price_df.iloc[0][filed]
+            x_day_change_price = stock_latest_price - stock_x_day_price
+            x_day_change_percent = round(x_day_change_price / stock_x_day_price * 100, 2)
 
-              x_day_change_price = stock_latest_price - stock_x_day_price
-              x_day_change_percent = round(x_day_change_price / stock_x_day_price * 100, 2)
-
-              column_data.append(x_day_change_percent)
-              # print(f'LATEST: {stock_latest_price} | {day} days ago: {stock_x_day_price} / {x_day_change_percent}%')
-            else:
-              x_day_change_percent = 0
-              column_data.append(x_day_change_percent)
-          
-          stocks_change_data.append(column_data)
+            column_data.append(x_day_change_percent)
+            # print(f'LATEST: {stock_latest_price} | {day} days ago: {stock_x_day_price} / {x_day_change_percent}%')
+          else:
+            x_day_change_percent = 0
+            column_data.append(x_day_change_percent)
         
-        df = pd.DataFrame(np.array(stocks_change_data), columns=db_columns)
-        df.to_sql('change_pct', change_db_conn, if_exists="replace")
+        stocks_change_data.append(column_data)
+  df = pd.DataFrame(np.array(stocks_change_data), columns=db_columns)
+
+  # change_db_dir_path = '../database/rps/'
+  # change_db_file = 'all_change'
+
+  # if not os.path.exists(change_db_dir_path):
+  #   print('rps dir not exist, create one')
+  #   os.mkdir(change_db_dir_path)
+  # with sql.connect(change_db_dir_path + change_db_file + '.db') as change_db_conn:
+  #   df.to_sql('change_pct', change_db_conn, if_exists="replace")
+
+  return df
+  
+if __name__=='__main__':
+  # 计算所有股票的不同日子涨跌幅百分比
+  RPS_DAYS = (5, 10, 20, 60, 120, 250)
+  price_change_df = cal_price_change(RPS_DAYS)
+
+  # 筛选出 ST 和 上市不满一年的股票列表
+  with sql.connect('../database/stock_a.db') as conn:
+    tables = ('sh_zhuban', 'sh_kechuangban', 'sz_zhuban', 'sz_chuangyeban')
+    vectfunc = np.vectorize(get_new_and_st, cache=False)
+    stocks_ndarray = vectfunc(tables, conn)
+
+  # 从所有股票的涨跌值表中去除 ST 和上市不满一年的
+  for bk in stocks_ndarray:
+    remove_list = bk['company_code'].values.tolist()
+    price_change_df = price_change_df[~price_change_df['company_code'].isin(remove_list)]
+
+  with sql.connect('../database/rps/price_change_no_new_st.db') as pc_test_conn:
+    price_change_df.to_sql('test', pc_test_conn, if_exists='replace')
+
+  # 去除后的股票数量
+  stock_count = price_change_df.shape[0]
+  # 初始化 rps 数据 df
+  all_rps_df = pd.DataFrame()
+
+  for index_rps, rps in enumerate(RPS_DAYS):
+    # 因为记录的时候是字符串，要转换为浮点数
+    price_change_df[f'change_{rps}_pct'] = price_change_df[f'change_{rps}_pct'].astype(float)
+    # 按 x 日的涨跌幅百分比排序倒序，重置索引数，赋值到新的 Dataframe
+    new_df = price_change_df.sort_values(by=[f'change_{rps}_pct'], ascending=False).reset_index(drop=True)
+    
+    company_code = []
+    rps_x = []
+    # 行遍历新的 Dataframe，计算排名标准分，就是 RPS 指标
+    for index, row in new_df.iterrows():
+      # print(f'{row["company_code"]} | RPS_{rps}: {round((1 - (index + 1) / stock_count) * 100, 2)}%')
+      company_code.append(row['company_code'])
+      rps_x.append(round((1 - (index + 1) / stock_count) * 100, 2))
+      # with sql.connect('../database/rps/test.db') as rps_test_conn:
+      #   new_df.to_sql('test', rps_test_conn, if_exists='replace')
+    
+    rps_x_df = pd.DataFrame({'company_code': company_code, f'rps_{rps}': rps_x})
+    print(index_rps)
+    if index_rps == 0:
+      all_rps_df = rps_x_df
+    else:
+      all_rps_df = pd.merge(all_rps_df, rps_x_df, on=['company_code'])
+  
+  print(all_rps_df)
+  today = datetime.today().strftime('%Y%m%d')
+  all_rps_df.to_excel(f'../database/rps/rps_{today}.xlsx')
+  # print(new_df[['company_code', 'change_5_pct']])
+    # print(bk)
+    # print(type(bk['company_code']))
--- back/remove_new_and_st.py
+++ back/remove_new_and_st.py
@@ -9,7 +9,10 @@ import numpy as np
 # 从数据库中读取上市时间大于 1 年的股票且不为 ST
 def get_all_stock_without_new_and_st(table, db_conn):
   fields = '*'
-  conditions = 'listing_date < date("now","-1 years") AND instr(company_abbr, "ST") = 0'
+  # 筛选出不是一年内的新股且不为 ST
+  # conditions = 'listing_date < date("now","-1 years") AND instr(company_abbr, "ST") = 0'
+  # 筛选出上市不足一年或 ST
+  conditions = 'listing_date > date("now","-1 years") OR instr(company_abbr, "ST") = 1'
   sql_string = (f'SELECT {fields} '
                 f'FROM {table} '
                 f'WHERE {conditions};')
@@ -22,10 +25,10 @@ with sql.connect('database/stock_a.db') as conn:
   tables = ('sh_zhuban', 'sh_kechuangban', 'sz_zhuban', 'sz_chuangyeban')
 
   vectfunc = np.vectorize(get_all_stock_without_new_and_st, cache=False)
-  a = vectfunc(tables, conn)
+  stocks_ndarray = vectfunc(tables, conn)
 
-  print(a)
-
-  # with sql.connect('database/test.db') as test:
-  #   sh_zhuban_without_new.to_sql("sh_zhuban_without_new", test, if_exists='replace')
+  for bk in stocks_ndarray:
+    # print(bk[['company_code','company_abbr']])
+    # print(bk)
+    print(type(bk['company_code']))
 
